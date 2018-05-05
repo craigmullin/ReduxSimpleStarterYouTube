400b90be1bfa3763d65c08eb170d1b07d444792a
Craig Mullin <cmullin@dreeshomes.com>
2018-05-05 11:45:19 -0400

    thru lesson 31

--- src/index.js
+++ src/index.js
@@ -26,19 +26,32 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            seleectedVideo: null
+         };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
             // same as this.setState({ videos: videos }); - when key and variable have same name
         });
     }
+
     render() {
         return (
             <div>
-                <SearchBar />
-                <VideoDetail video={this.state.videos[0]}/>
-                <VideoList videos={this.state.videos} />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                    videos={this.state.videos} />
             </div>
         ); 
     }
